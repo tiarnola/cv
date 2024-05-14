e9b6e14ce2303d9233e0b2c1dd1513448621960d
Tiarnan Guinee <tiarnanguinee@Tiarnans-MacBook-Pro-2.local>
2024-05-14 10:27:48 +0100

    Remove PDF, add PNGs

--- src/pages/CV/CV.tsx
+++ src/pages/CV/CV.tsx
@@ -1,14 +1,18 @@
-import pdf from "../../assets/pdf/Tiarnan Guinee Curriculum Vitae.pdf"
+// import pdf from "../../assets/pdf/Tiarnan Guinee Curriculum Vitae.pdf"
+
+import "./CV.css"
+
+import page1 from "../../assets/img/Tiarnan Guinee Curriculum Vitae_Page_1.png";
+import page2 from "../../assets/img/Tiarnan Guinee Curriculum Vitae_Page_2.png";
 
 export const CV = () => {
 
   return (
+    <>
     <div>
-      <iframe
-        title="Curriculum Vitae"
-        src={pdf}
-        style={{ width: "100%", height: "100vw" }}
-      />
+      <img src={page1} className="png-viewer"></img>
+      <img src={page2} className="png-viewer"></img>
     </div>
+    </>
   );
 };
